0cdb79f738e7579698724516d54b9f5477ffd785
LuChronu <lc.oficial14@gmail.com>
2026-03-04 21:58:31 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = "https://ftjzhxbempifcxnrxhoh.supabase.co"
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0anpoeGJlbXBpZmN4bnJ4aG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MDQwODcsImV4cCI6MjA4ODE4MDA4N30.DFQsTZa5rFcDAw-gKJZJTRj1kPj-h4gTt5jq7ExRXNQ"
+const SUPABASE_KEY = "sb_publishable_2GRy6GmkuJAbbiSo367iFQ_d0naHmY0"
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
